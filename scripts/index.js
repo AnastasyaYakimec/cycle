e56feab79a869c788ce98db2a3cf4c6c0ff353af
Anastasya Yakimec <youremail@gmail.com>
2022-06-02 18:32:16 +0300

    add ex_2_for

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+
+// for (let i = 0; i <= 100; i++){
+//     if (i % 2 !== 0) continue;
+//     alert(i);
+// }
+// for (let i = 0; i <= 100; i++){
+//     if (i % 3 !== 0) continue;
+//     alert(i);
+// }
+
+
+// let ageUser = 18;
+// switch (ageUser) {
+//     case 18 :
+//         alert("yes");
+//         break;   
+//     case ageUser < 18 :
+//         alert("no");
+//         break;  
+// }
+
